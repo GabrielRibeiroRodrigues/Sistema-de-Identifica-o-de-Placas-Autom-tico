85fb12b1e121b4e0455c2a07a80b421438cc7040
Gabriel Ribeiro Rodrigues <103051424+GabrielRibeiroRodrigues@users.noreply.github.com>
2025-02-19 20:24:49 -0300

    Para Testes

--- Projetoff_ver/reconhecimento_de_placas/main.py
+++ Projetoff_ver/reconhecimento_de_placas/main.py
@@ -4,101 +4,103 @@ import numpy as np
 import matplotlib.pyplot as plt
 import keyboard  
 from sort.sort import Sort
-from util import get_car, read_license_plate, write_csv, carrega_placas_registradas
+from util import ler_carro, ler_placas, escrever_csv, carrega_placas_registradas
 
 results = {}
 mot_tracker = Sort()
 
-
 placas_registradas = carrega_placas_registradas('C:\\Users\\Pichau\\Desktop\\Projetoff_ver\\planilha.csv')
 detector_carro = YOLO('yolov8n.pt')
 detector_placa = YOLO("C:\\Users\\Pichau\\Desktop\\best (4).pt")
-cap = cv2.VideoCapture("C:\\Users\\Pichau\\Desktop\\ffff.mp4")
+cap = cv2.VideoCapture("C:\\Users\\Pichau\\Desktop\\granato.mp4")
 
 veiculos = [2, 3, 5, 7]  
 confianca_detectar_carro = 0.0  
-confianca_gravar_texto = 0.0
+confianca_gravar_texto = 0.1
 maior_confianca = 0.0
 frame_nmr = -1
 ret = True
-
+intervalo_frames = 1 # Lugar onde eu defino o intervalo de frames que o algoritmo va ler
 
 plt.ion()  
 fig, ax = plt.subplots()
 
 
 while ret:
-    frame_nmr += 1
-    ret, frame = cap.read()  
-    if not ret:  
-        print("Não foi possível ler o frame.")
-        break
-
+    for i in range(intervalo_frames):
+        frame_nmr += 1
+        ret, frame = cap.read()
+        if not ret or frame is None:
+            print(f"Não foi possível ler o frame {frame_nmr}.")
+            break
+    if frame is None:
+        continue
     results[frame_nmr] = {}
 
     # Detecção de veículos usando o modelo de veículos
     detections_veiculos = detector_carro(frame)[0]
     veiculos_detectados = []
     for detection in detections_veiculos.boxes.data.tolist():
-        x1, y1, x2, y2, score, class_id = detection
-        if score >= confianca_detectar_carro and int(class_id) in veiculos:
-            veiculos_detectados.append([x1, y1, x2, y2, score])
-    print(f"Frame {frame_nmr} - Veículos detectados: {veiculos_detectados}")
+        x1, y1, x2, y2, confianca_atual, class_id = detection
+        if confianca_atual >= confianca_detectar_carro and int(class_id) in veiculos:
+            veiculos_detectados.append([x1, y1, x2, y2, confianca_atual])
+    print(f"Frame {frame_nmr}")
 
     # Rastrear veículos
     if veiculos_detectados:
         track_ids = mot_tracker.update(np.asarray(veiculos_detectados))
     else:
         track_ids = []
-        print(f"Frame {frame_nmr} - Nenhum veículo detectado")
+        
 
     # Detecção de placas usando o modelo de placas
     detections_placas = detector_placa(frame)[0]
     placas_detectadas = []
     for detection in detections_placas.boxes.data.tolist():
-        x1, y1, x2, y2, score, class_id = detection
-        if score >= confianca_detectar_carro:
-            placas_detectadas.append([x1, y1, x2, y2, score])
-    print(f"Frame {frame_nmr} - Placas detectadas: {placas_detectadas}")
+        x1, y1, x2, y2, confianca_atual, class_id = detection
+        if confianca_atual >= confianca_detectar_carro:
+            placas_detectadas.append([x1, y1, x2, y2, confianca_atual])
+    print(f"Frame {frame_nmr}")
 
     # Atribuir as placas aos veículos detectados
     for placa in placas_detectadas:
-        x1, y1, x2, y2, score = placa
-        print(f"Placa detectada no frame {frame_nmr} com coordenadas: ({x1}, {y1}), ({x2}, {y2}) e confiança {score}")
+        x1, y1, x2, y2, confianca_atual = placa
+
 
         # Verificar qual veículo corresponde à placa
-        xcar1, ycar1, xcar2, ycar2, car_id = get_car(placa, track_ids)
+        xcar1, ycar1, xcar2, ycar2, car_id = ler_carro(placa, track_ids)
 
         if car_id != -1:
             # Verificação de limites
             if (0 <= x1 < frame.shape[1] and 0 <= x2 < frame.shape[1] and
                 0 <= y1 < frame.shape[0] and 0 <= y2 < frame.shape[0]):
                 # Recortar a placa para processamento
-                license_plate_crop = frame[int(y1):int(y2), int(x1):int(x2)]
-                license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
-                _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray        , 64, 255, cv2.THRESH_BINARY_INV)
+                placa_carro_crop = frame[int(y1):int(y2), int(x1):int(x2)]
+                placa_carro_crop_gray = cv2.cvtColor(placa_carro_crop, cv2.COLOR_BGR2GRAY)
+                _, placa_carro_crop_thresh = cv2.threshold(placa_carro_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
 
                 # Ler o texto da placa
-                license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
-                print(f"Texto da placa detectado: {license_plate_text}, Confiança: {license_plate_text_score}")
+                texto_detectado, confianca_texto_detectado = ler_placas(placa_carro_crop_thresh)
+                
 
-                if license_plate_text is not None and license_plate_text_score > confianca_gravar_texto  :
-                    # maior_confianca = license_plate_text_score
+                if texto_detectado is not None and confianca_texto_detectado > confianca_gravar_texto  :
+                    
                     # Verificar se a placa já está registrada
-                    if license_plate_text in placas_registradas:
-                        info = placas_registradas[license_plate_text]
-                        print(f"A placa {license_plate_text} já está registrada.")
+                    if texto_detectado in placas_registradas:
+                        info = placas_registradas[texto_detectado]
+                        print("\033[92m" + "█" * 50 + "\033[0m")
+                        print(f"A placa  já está registrada.")
                         print(f"Proprietário: {info['proprietario']}, Veículo: {info['veiculo']}, Cor do Veículo: {info['cor']}")
                     else:
-                        print(f"A placa {license_plate_text} não está registrada.")
+                        print(f"A placa {texto_detectado} não está registrada.")
 
                     results[frame_nmr][car_id] = {
                         'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
                         'placa': {
                             'bbox': [x1, y1, x2, y2],
-                            'text': license_plate_text,
-                            'bbox_score': score,
-                            'text_score': license_plate_text_score
+                            'text': texto_detectado,
+                            'bbox_score': confianca_atual,
+                            'text_score': confianca_texto_detectado
                         }
                     }
                 else:
@@ -120,8 +122,8 @@ while ret:
         ax.add_patch(rect)
 
     # Desenhar bounding boxes para placas detectadas
-    for plate in placas_detectadas:
-        x1, y1, x2, y2, score = plate
+    for placa in placas_detectadas:
+        x1, y1, x2, y2, score = placa
         rect = plt.Rectangle((x1, y1), x2 - x1, y2 - y1, fill=False, color='red', linewidth=2)
         ax.add_patch(rect)
 
@@ -133,7 +135,7 @@ while ret:
 
    
         
-write_csv(results, 'C:\\Users\\Pichau\\Desktop\\Projetoff_ver\\test.csv')
+escrever_csv(results, 'C:\\Users\\Pichau\\Desktop\\Projetoff_ver\\test.csv')
 
 
 cap.release()
