0fc7040b7f94abb18fb6fdc73b2b55cb41bc9538
Gabriel Ribeiro Rodrigues <103051424+GabrielRibeiroRodrigues@users.noreply.github.com>
2025-03-11 08:47:57 -0300

    Add files via upload

--- Projetoff_ver/reconhecimento_de_placas/main.py
+++ Projetoff_ver/reconhecimento_de_placas/main.py
@@ -4,103 +4,106 @@ import numpy as np
 import matplotlib.pyplot as plt
 import keyboard  
 from sort.sort import Sort
-from util import ler_carro, ler_placas, escrever_csv, carrega_placas_registradas
+from util import get_car, read_license_plate, write_csv, carrega_placas_registradas
 
 results = {}
 mot_tracker = Sort()
 
-placas_registradas = carrega_placas_registradas('C:\\Users\\Pichau\\Desktop\\Projetoff_ver\\planilha.csv')
+placas_registradas = carrega_placas_registradas('C:\\Users\\Yasmin Pereira\\Desktop\\Projetoff_ver\\planilha.csv')
 detector_carro = YOLO('yolov8n.pt')
-detector_placa = YOLO("C:\\Users\\Pichau\\Desktop\\best (4).pt")
-cap = cv2.VideoCapture("C:\\Users\\Pichau\\Desktop\\granato.mp4")
-
+detector_placa = YOLO("C:\\Users\\Yasmin Pereira\\Desktop\\best (4).pt")
+cap = cv2.VideoCapture("rtsp://admin:123456789abc@192.168.0.2:554/cam/realmonitor?channel=1&subtype=0")
+# cap = cv2.VideoCapture("C:\\Users\\Pichau\\Desktop\\ffff.mp4")
 veiculos = [2, 3, 5, 7]  
 confianca_detectar_carro = 0.0  
-confianca_gravar_texto = 0.1
+confianca_gravar_texto = 0.0
 maior_confianca = 0.0
 frame_nmr = -1
 ret = True
-intervalo_frames = 1 # Lugar onde eu defino o intervalo de frames que o algoritmo va ler
 
 plt.ion()  
 fig, ax = plt.subplots()
-
+skip_frames = 3
 
 while ret:
-    for i in range(intervalo_frames):
+    for i in range(skip_frames):
         frame_nmr += 1
         ret, frame = cap.read()
+
+        # Verificação adicional para garantir que o frame não está vazio
         if not ret or frame is None:
             print(f"Não foi possível ler o frame {frame_nmr}.")
             break
+
     if frame is None:
-        continue
+        continue  # Pular a iteração se o frame for nulo
+
     results[frame_nmr] = {}
 
     # Detecção de veículos usando o modelo de veículos
     detections_veiculos = detector_carro(frame)[0]
     veiculos_detectados = []
     for detection in detections_veiculos.boxes.data.tolist():
-        x1, y1, x2, y2, confianca_atual, class_id = detection
-        if confianca_atual >= confianca_detectar_carro and int(class_id) in veiculos:
-            veiculos_detectados.append([x1, y1, x2, y2, confianca_atual])
-    print(f"Frame {frame_nmr}")
+        x1, y1, x2, y2, score, class_id = detection
+        if score >= confianca_detectar_carro and int(class_id) in veiculos:
+            veiculos_detectados.append([x1, y1, x2, y2, score])
+    print(f"Frame {frame_nmr} - Veículos detectados: {veiculos_detectados}")
 
     # Rastrear veículos
     if veiculos_detectados:
         track_ids = mot_tracker.update(np.asarray(veiculos_detectados))
     else:
         track_ids = []
-        
+        print(f"Frame {frame_nmr} - Nenhum veículo detectado")
 
     # Detecção de placas usando o modelo de placas
     detections_placas = detector_placa(frame)[0]
     placas_detectadas = []
     for detection in detections_placas.boxes.data.tolist():
-        x1, y1, x2, y2, confianca_atual, class_id = detection
-        if confianca_atual >= confianca_detectar_carro:
-            placas_detectadas.append([x1, y1, x2, y2, confianca_atual])
-    print(f"Frame {frame_nmr}")
+        x1, y1, x2, y2, score, class_id = detection
+        if score >= confianca_detectar_carro:
+            placas_detectadas.append([x1, y1, x2, y2, score])
+    print(f"Frame {frame_nmr} - Placas detectadas: {placas_detectadas}")
 
     # Atribuir as placas aos veículos detectados
     for placa in placas_detectadas:
-        x1, y1, x2, y2, confianca_atual = placa
-
+        x1, y1, x2, y2, score = placa
+        print(f"Placa detectada no frame {frame_nmr} com coordenadas: ({x1}, {y1}), ({x2}, {y2}) e confiança {score}")
 
         # Verificar qual veículo corresponde à placa
-        xcar1, ycar1, xcar2, ycar2, car_id = ler_carro(placa, track_ids)
+        xcar1, ycar1, xcar2, ycar2, car_id = get_car(placa, track_ids)
 
         if car_id != -1:
             # Verificação de limites
             if (0 <= x1 < frame.shape[1] and 0 <= x2 < frame.shape[1] and
                 0 <= y1 < frame.shape[0] and 0 <= y2 < frame.shape[0]):
                 # Recortar a placa para processamento
-                placa_carro_crop = frame[int(y1):int(y2), int(x1):int(x2)]
-                placa_carro_crop_gray = cv2.cvtColor(placa_carro_crop, cv2.COLOR_BGR2GRAY)
-                _, placa_carro_crop_thresh = cv2.threshold(placa_carro_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
+                license_plate_crop = frame[int(y1):int(y2), int(x1):int(x2)]
+                license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
+                _, license_plate_crop_thresh = cv2.threshold(license_plate_crop_gray, 64, 255, cv2.THRESH_BINARY_INV)
 
                 # Ler o texto da placa
-                texto_detectado, confianca_texto_detectado = ler_placas(placa_carro_crop_thresh)
-                
+                license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
+                print(f"Texto da placa detectado: {license_plate_text}, Confiança: {license_plate_text_score}")
 
-                if texto_detectado is not None and confianca_texto_detectado > confianca_gravar_texto  :
-                    
+                if license_plate_text is not None and license_plate_text_score > confianca_gravar_texto:
+                    # Nova barra verde no console quando o texto for identificado
+                    print("\033[42m" + f" PLACA RECONHECIDA: {license_plate_text} " + "\033[0m")
                     # Verificar se a placa já está registrada
-                    if texto_detectado in placas_registradas:
-                        info = placas_registradas[texto_detectado]
-                        print("\033[92m" + "█" * 50 + "\033[0m")
-                        print(f"A placa  já está registrada.")
+                    if license_plate_text in placas_registradas:
+                        info = placas_registradas[license_plate_text]
+                        print(f"A placa {license_plate_text} já está registrada.")
                         print(f"Proprietário: {info['proprietario']}, Veículo: {info['veiculo']}, Cor do Veículo: {info['cor']}")
                     else:
-                        print(f"A placa {texto_detectado} não está registrada.")
+                        print(f"A placa {license_plate_text} não está registrada.")
 
                     results[frame_nmr][car_id] = {
                         'car': {'bbox': [xcar1, ycar1, xcar2, ycar2]},
                         'placa': {
                             'bbox': [x1, y1, x2, y2],
-                            'text': texto_detectado,
-                            'bbox_score': confianca_atual,
-                            'text_score': confianca_texto_detectado
+                            'text': license_plate_text,
+                            'bbox_score': score,
+                            'text_score': license_plate_text_score
                         }
                     }
                 else:
@@ -110,33 +113,32 @@ while ret:
         else:
             print("Nenhum veículo correspondente à placa foi detectado.")
 
-   
-    ax.clear()
-    ax.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))  
-    ax.axis('off')  
+    # Verificação de frame vazio novamente antes de mostrar
+    if frame is not None:
+        ax.clear()
+        ax.imshow(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))  
+        ax.axis('off')
 
-    # Desenhar bounding boxes para veículos detectados
-    for veh in veiculos_detectados:
-        x1, y1, x2, y2, score = veh
-        rect = plt.Rectangle((x1, y1), x2 - x1, y2 - y1, fill=False, color='blue', linewidth=2)
-        ax.add_patch(rect)
+        # Desenhar bounding boxes para veículos detectados
+        for veh in veiculos_detectados:
+            x1, y1, x2, y2, score = veh
+            rect = plt.Rectangle((x1, y1), x2 - x1, y2 - y1, fill=False, color='blue', linewidth=2)
+            ax.add_patch(rect)
 
-    # Desenhar bounding boxes para placas detectadas
-    for placa in placas_detectadas:
-        x1, y1, x2, y2, score = placa
-        rect = plt.Rectangle((x1, y1), x2 - x1, y2 - y1, fill=False, color='red', linewidth=2)
-        ax.add_patch(rect)
+        # Desenhar bounding boxes para placas detectadas
+        for plate in placas_detectadas:
+            x1, y1, x2, y2, score = plate
+            rect = plt.Rectangle((x1, y1), x2 - x1, y2 - y1, fill=False, color='red', linewidth=2)
+            ax.add_patch(rect)
 
-    plt.pause(0.00001)  # Pausa para atualizar a visualização
+        plt.pause(0.00001)  # Pausa para atualizar a visualização
 
     # Verificar se a tecla 'q' foi pressionada
     if keyboard.is_pressed('q'):
         break  # Encerra o loop
 
-   
-        
-escrever_csv(results, 'C:\\Users\\Pichau\\Desktop\\Projetoff_ver\\test.csv')
-
+# Salvar os resultados no CSV
+write_csv(results, 'C:\\Users\\Yasmin Pereira\\Desktop\\Projetoff_ver\\test.csv')
 
 cap.release()
 plt.close(fig)
--- Projetoff_ver/reconhecimento_de_placas/util.py
+++ Projetoff_ver/reconhecimento_de_placas/util.py
@@ -8,7 +8,7 @@ reader = easyocr.Reader(['en'], gpu=False)
 
 
 #Conversões de caracteres nao esperados para esperados se estiver na lista
-char_to_int = {'O': '0',
+dict_char_to_int = {'O': '0',
                     'I': '1',
                     'J': '3',
                     'A': '4',
@@ -16,16 +16,16 @@ char_to_int = {'O': '0',
                     'S': '5',
                     }
 
-int_to_char = {'0': 'O',
+dict_int_to_char = {'0': 'O',
                     '1': 'I',
                     '3': 'J',
                     '4': 'A',
                     '6': 'G',
                     '5': 'S',
-                    }
+                    'H': 'W'}
 
 
-def escrever_csv(results, output_path):
+def write_csv(results, output_path):
     with open(output_path, 'w') as f:
         f.write('{},{},{},{},{}\n'.format('frame_nmr', 'car_id',
                                                 'license_plate_bbox_score', 'license_number',
@@ -33,7 +33,7 @@ def escrever_csv(results, output_path):
 
         for frame_nmr in results.keys():
             for car_id in results[frame_nmr].keys():
-                
+                print(results[frame_nmr][car_id])
                 if 'car' in results[frame_nmr][car_id].keys() and \
                    'placa' in results[frame_nmr][car_id].keys() and \
                    'text' in results[frame_nmr][car_id]['placa'].keys():
@@ -47,6 +47,8 @@ def escrever_csv(results, output_path):
         f.close()
 
 
+import re
+
 def license_complies_format(text):
       
     if len(text) != 7:
@@ -56,73 +58,77 @@ def license_complies_format(text):
     
 
     # Teste
-    if (text[0] in string.ascii_uppercase or text[0] in int_to_char.keys()) and \
-       (text[1] in string.ascii_uppercase or text[1] in int_to_char.keys()) and \
-       (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in char_to_int.keys()) and \
-       (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in char_to_int.keys()) and \
-       (text[4] in string.ascii_uppercase or text[4] in int_to_char.keys()) and \
-       (text[5] in string.ascii_uppercase or text[5] in int_to_char.keys()) and \
-       (text[6] in string.ascii_uppercase or text[6] in int_to_char.keys()):
+    if (text[0] in string.ascii_uppercase or text[0] in dict_int_to_char.keys()) and \
+       (text[1] in string.ascii_uppercase or text[1] in dict_int_to_char.keys()) and \
+       (text[2] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[2] in dict_char_to_int.keys()) and \
+       (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
+       (text[4] in string.ascii_uppercase or text[4] in dict_int_to_char.keys()) and \
+       (text[5] in string.ascii_uppercase or text[5] in dict_int_to_char.keys()) and \
+       (text[6] in string.ascii_uppercase or text[6] in dict_int_to_char.keys()):
         return True
      
 
     #Formato padrão
-    if (text[0] in string.ascii_uppercase or text[0] in int_to_char.keys()) and \
-       (text[1] in string.ascii_uppercase or text[1] in int_to_char.keys()) and \
-       (text[2] in string.ascii_uppercase or text[2] in int_to_char.keys()) and \
-       (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in char_to_int.keys()) and \
-       (text[4] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[4] in char_to_int.keys()) and \
-       (text[5] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[5] in char_to_int.keys()) and \
-       (text[6] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[6] in char_to_int.keys()):
+    if (text[0] in string.ascii_uppercase or text[0] in dict_int_to_char.keys()) and \
+       (text[1] in string.ascii_uppercase or text[1] in dict_int_to_char.keys()) and \
+       (text[2] in string.ascii_uppercase or text[2] in dict_int_to_char.keys()) and \
+       (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
+       (text[4] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[4] in dict_char_to_int.keys()) and \
+       (text[5] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[5] in dict_char_to_int.keys()) and \
+       (text[6] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[6] in dict_char_to_int.keys()):
         return True
     #Formato Mercosul
-    if (text[0] in string.ascii_uppercase or text[0] in int_to_char.keys()) and \
-       (text[1] in string.ascii_uppercase or text[1] in int_to_char.keys()) and \
-       (text[2] in string.ascii_uppercase or text[2] in int_to_char.keys()) and \
-       (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in char_to_int.keys()) and \
-       (text[4] in string.ascii_uppercase or text[4] in int_to_char.keys()) and \
-       (text[5] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[5] in char_to_int.keys()) and \
-       (text[6] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[6] in char_to_int.keys()):
+    if (text[0] in string.ascii_uppercase or text[0] in dict_int_to_char.keys()) and \
+       (text[1] in string.ascii_uppercase or text[1] in dict_int_to_char.keys()) and \
+       (text[2] in string.ascii_uppercase or text[2] in dict_int_to_char.keys()) and \
+       (text[3] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[3] in dict_char_to_int.keys()) and \
+       (text[4] in string.ascii_uppercase or text[4] in dict_int_to_char.keys()) and \
+       (text[5] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[5] in dict_char_to_int.keys()) and \
+       (text[6] in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] or text[6] in dict_char_to_int.keys()):
         return True
     
 
     return False
 
-def formato_placa(text):
+def format_license(text):
     license_plate_ = ''
 
+    # O mapeamento agora depende da posição dos caracteres
     for j in range(7):
+        # Se estivermos nas posições das letras (para ambos os formatos)
         if j in [0, 1, 2]:
-            if text[j] in int_to_char:
-                license_plate_ += int_to_char[text[j]]
+            # Tentar mapear o caractere como uma letra, usando o dicionário de conversão de números para letras
+            if text[j] in dict_int_to_char:
+                license_plate_ += dict_int_to_char[text[j]]
             else:
                 license_plate_ += text[j]
+
         # Se estivermos nas posições que podem ser números (para ambos os formatos)
         elif j == 3:
             # Posição 3 pode ser um número em ambos os formatos
-            if text[j] in char_to_int:
-                license_plate_ += char_to_int[text[j]]
+            if text[j] in dict_char_to_int:
+                license_plate_ += dict_char_to_int[text[j]]
             else:
                 license_plate_ += text[j]
 
         # Posição 4 pode ser uma letra (no formato `AAA1A23`) ou um número (no formato `AAA1234`)
         elif j == 4:
-            if text[j] in string.ascii_uppercase or text[j] in int_to_char:
+            if text[j] in string.ascii_uppercase or text[j] in dict_int_to_char:
                 # Mapear letra para número, se aplicável
-                if text[j] in int_to_char:
-                    license_plate_ += int_to_char[text[j]]
+                if text[j] in dict_int_to_char:
+                    license_plate_ += dict_int_to_char[text[j]]
                 else:
                     license_plate_ += text[j]
-            elif text[j] in char_to_int:
+            elif text[j] in dict_char_to_int:
                 # Mapear número para letra, se aplicável
-                license_plate_ += char_to_int[text[j]]
+                license_plate_ += dict_char_to_int[text[j]]
             else:
                 license_plate_ += text[j]
 
         # Posições 5 e 6 sempre podem ser números (em ambos os formatos)
         elif j in [5, 6]:
-            if text[j] in char_to_int:
-                license_plate_ += char_to_int[text[j]]
+            if text[j] in dict_char_to_int:
+                license_plate_ += dict_char_to_int[text[j]]
             else:
                 license_plate_ += text[j]
 
@@ -130,7 +136,7 @@ def formato_placa(text):
 
 
 #Lê os caracteres que estão na placa
-def ler_placas(license_plate_crop):
+def read_license_plate(license_plate_crop):
     detections = reader.readtext(license_plate_crop)
 
     for detection in detections:
@@ -139,12 +145,12 @@ def ler_placas(license_plate_crop):
         text = text.upper().replace(' ', '')
 
         if license_complies_format(text):
-            return formato_placa(text), score
+            return format_license(text), score
 
     return None, None
 
 
-def ler_carro(placa, vehicle_track_ids):
+def get_car(placa, vehicle_track_ids):
     x1, y1, x2, y2, score, class_id = (*placa, None)
 
     foundIt = False
@@ -167,4 +173,5 @@ def carrega_placas_registradas(csv_path):
     for index, row in placas_reg.iterrows():
           placas_dict[row['placa']] = {'proprietario': row['proprietario'], 'veiculo': row['veiculo'],'cor': row['cor']}
     return placas_dict
+
 
